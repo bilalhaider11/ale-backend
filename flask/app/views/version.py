8f5f14437787fb21fd2933ebde0eee2fa10e7e76
Ahmad <ahmadhassanminnhas@gmail.com>
2025-05-19 23:06:20 +0500

    Add version API endpoint

---
+++ flask/app/views/version.py
@@ -0,0 +1,21 @@
+from flask_restx import Namespace, Resource
+from app.helpers.response import get_success_response
+import toml
+
+
+# Create the version namespace
+version_api = Namespace('version', description="Version-related APIs")
+
+@version_api.route('/')
+class Version(Resource):
+    def get(self):
+
+        # Read the version from pyproject.toml
+        try:
+            with open("pyproject.toml", "r") as toml_file:
+                pyproject = toml.load(toml_file)
+            VERSION = pyproject["tool"]["poetry"]["version"]
+        except Exception as e:
+            VERSION = f"Error reading version: {str(e)}"
+
+        return get_success_response(version=VERSION)
