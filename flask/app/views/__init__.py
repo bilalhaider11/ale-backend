670b87b168525f308018c6bfaea2ae468f198a78
Ahmad <ahmadhassanminnhas@gmail.com>
2025-06-16 16:08:26 +0500

    Add endpoint to upload current caregiver CSV file and store in S3

--- flask/app/views/__init__.py
+++ flask/app/views/__init__.py
@@ -2,11 +2,13 @@ from app.views.auth import auth_api
 from app.views.organization import organization_api
 from app.views.person import person_api
 from app.views.version import version_api
-from app.views.employee import employee_api
+from app.views.current_employee import current_employee_api
+from app.views.current_caregiver import current_caregiver_api
 
 def initialize_views(api):
     api.add_namespace(auth_api)
     api.add_namespace(organization_api)
     api.add_namespace(person_api)
     api.add_namespace(version_api)
-    api.add_namespace(employee_api)
+    api.add_namespace(current_employee_api)
+    api.add_namespace(current_caregiver_api)
