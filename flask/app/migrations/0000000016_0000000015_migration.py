97957a75062fc8ee3483d2ed7aa1c8eef031a8c0
asymness <asym1999@gmail.com>
2025-06-30 23:19:21 +0500

    Update bug in migration

--- flask/app/migrations/0000000016_0000000015_migration.py
+++ flask/app/migrations/0000000016_0000000015_migration.py
@@ -10,21 +10,11 @@ def upgrade(migration):
         datatype="VARCHAR(32) DEFAULT NULL"
     )
     
-    # Add social_security_number column to current_employee_audit table
-    migration.add_column(
-        table_name="current_employee_audit", 
-        column_name="social_security_number", 
-        datatype="VARCHAR(32) DEFAULT NULL"
-    )
-    
     migration.update_version_table(version=revision)
 
 
 def downgrade(migration):
     # Remove social_security_number column from current_employee table
     migration.drop_column("current_employee", "social_security_number")
-    
-    # Remove social_security_number column from current_employee_audit table
-    migration.drop_column("current_employee_audit", "social_security_number")
     
     migration.update_version_table(version=down_revision)
