b0f62909f73fd62bbd7c304c70b513d13ea0671b
Ahmad <ahmadhassanminnhas@gmail.com>
2025-08-04 19:40:33 +0500

    Truncate first_name and last_name values to 128 characters

--- common/models/person.py
+++ common/models/person.py
@@ -4,3 +4,17 @@ from typing import ClassVar
 
 class Person(BasePerson):
     use_type_checking: ClassVar[bool] = True
+    
+    def validate_first_name(self):
+        """
+        Truncate first name to 128 characters.
+        """
+        if type(self.first_name) is str:
+            self.first_name = self.first_name[:128]
+    
+    def validate_last_name(self):
+        """
+        Truncate last name to 128 characters.
+        """
+        if type(self.last_name) is str:
+            self.last_name = self.last_name[:128]
